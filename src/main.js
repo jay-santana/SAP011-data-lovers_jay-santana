b0010fa0df59cdecb60b91d5ec9add2ec2d1de77
biancaBezerra <bianca.skills@gmail.com>
2023-08-03 20:38:54 -0300

    Co-authored-by: jay-santana <jay-santana@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+
+const dataRM = data.results;
+
+function cardCharacters (dataRM) {
+    const printCard = dataRM.map((dataRM) => {
+        const card = `
+        <div class = "cards">
+            <img src = "${dataRM.image}" alt = "Characters image">
+            <h2> ${dataRM.name}</h2>
+            <h3> ${dataRM.type}</h3>
+            `;
+        return card;
+    });
+    document.getElementById("info-cards"). innerHTML = printCard.join("");
+
+}
+
+window.addEventListener('load', () => cardCharacters(dataRM))
 
 console.log(example, data);
