95c12c17971cf1df6f94095d5bffaa6fac42e7c8
biancaBezerra <bianca.skills@gmail.com>
2023-08-11 10:55:21 -0300

    ajuste do click do modal em cada card

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import { statusFilter, speciesFilter } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 
-const openCardButton = document.getElementById('info-cards');
 const closeCardButton = document.getElementById("close-modal");
 const modal = document.getElementById("modal");
 const fade = document.getElementById("fade");
 
-const toggleModal = () => {
+const toggleModal = (dataRM) => {
     modal.classList.toggle('hide');
     fade.classList.toggle('hide');
+    //passar aqui o parametro dataRM, DENTRO DA TOGGLE MODAL CRIAR A TEMPLATESTRING(PARECIDO COM CARDCHARACTER)
 }
 
-[openCardButton, closeCardButton, fade].forEach((el) => {
+[closeCardButton, fade].forEach((el) => {
     el.addEventListener('click', () => toggleModal());
 })
 
@@ -21,22 +21,26 @@ const dataRM = data.results;
 
 
 //função para trazer todos os personagens em tela
-function cardCharacters(dataRM) {
-    const printCard = dataRM.map((dataRM) => {
+function cardCharacters(data) {
+    const cardsContainer = document.getElementById('info-cards');
+    cardsContainer.innerHTML = "";
+    data.forEach((dataRM) => {
+        const container = document.createElement("div")
+        container.classList.add("style-container")
         const card = `
-        <div class="style-container">
-            <div class="border-container">
-                <div class = "cards">
-                    <img id="img-card" src = "${dataRM.image}" alt = "Characters image">
-                    <div class="name-container">
-                        <h2> ${dataRM.name}</h2>
-                    </div>
+        <div class="border-container">
+            <div class = "cards">
+                <img id="img-card" src = "${dataRM.image}" alt = "Characters image">
+                <div class="name-container">
+                    <h2> ${dataRM.name}</h2>
                 </div>
             </div>
         </div>`;
-        return card;    
+
+        container.innerHTML = card;
+        container.addEventListener("click", toggleModal);
+        cardsContainer.appendChild(container);
     });
-    document.getElementById("info-cards").innerHTML = printCard.join("");
 
 }
 
@@ -45,8 +49,7 @@ window.addEventListener('load', () => {
     cardCharacters(dataRM);
 
     const statusSelect = document.getElementById('status-filter');
-    const speciesSelect = document.getElementById('species-filter')
-    const cardsContainer = document.getElementById('info-cards');
+    const speciesSelect = document.getElementById('species-filter');
 
     statusSelect.addEventListener('change', applyFilters);
     speciesSelect.addEventListener('change', applyFilters);
@@ -58,25 +61,8 @@ window.addEventListener('load', () => {
         // Filtrar os dados com base no status e especie selecionados
         const filteredDataByStatus = statusFilter(dataRM, selectedStatus);
         const filteredDataBySpeciesAndStatus = speciesFilter(filteredDataByStatus, selectedSpecies);
+        cardCharacters(filteredDataBySpeciesAndStatus);
 
-        // Criar os cartões filtrados
-        const printCard = filteredDataBySpeciesAndStatus.map((dataRM) => {
-            const card = `
-            <div class="style-container">
-                <div class="border-container">
-                    <div class="cards">
-                        <img id="img-card" src="${dataRM.image}" alt="Characters image">
-                        <div class="name-container">
-                            <h2>${dataRM.name}</h2>
-                        </div>
-                    </div>    
-                </div>
-            </div>`;
-            return card;
-        });
-
-        // Atualizar o container de cartões com os cartões filtrados
-        cardsContainer.innerHTML = printCard.join("");
     }
 });
 
